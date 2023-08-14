b44d66b939b1c8a98423bc6ceb3fcb3ded167513
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-08-14 12:34:17 +0300

    created the project structure

---
+++ README.md
@@ -0,0 +1,1 @@
+Dictionary-web-app
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from "vite";
+import { glob } from "glob";
+import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
 
 export default defineConfig({
+  root: "src",
   base: "/dictionary-web-app/",
+  build: {
+    rollupOptions: {
+      input: glob.sync("./src/*.html"),
+    },
+    outDir: "../dist",
+  },
+  plugins: [injectHTML(), FullReload(["./src/**/**.html"])],
 });
